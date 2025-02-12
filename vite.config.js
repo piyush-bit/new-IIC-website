bd488174f9388df531d7e895f42993b98529b92e
Piyush <mr.piyush.cv@gmail.com>
2025-02-12 23:13:17 +0530

    schadcn init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // Alias "@" to "src" folder
+      "@components": path.resolve(__dirname, "./src/components"), // Alias for components
+    },
+  },
 })
