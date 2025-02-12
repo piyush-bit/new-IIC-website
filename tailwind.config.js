67069cc19597446ef914cf7fcc40681563525fbc
chinmay sahoo <chinmaysahoo668@gmail.com>
2025-02-13 00:29:42 +0530

    temp changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'heinch': ['Heinch-up','Heinch' , 'sans-serif'], // Use "sans-serif" as a fallback
-      },
-      colors: {
-        'primary': '#1d2a42', // You can name it as you like
-      },    
-    },
+  	extend: {
+  		fontFamily: {
+  			heinch: [
+  				'Heinch-up',
+  				'Heinch',
+  				'sans-serif'
+  			]
+  		},
+  		colors: {
+  			primary: '#1d2a42'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
